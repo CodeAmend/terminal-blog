048db38d9ad1b74a4113d514972291f1a462d030
Michael Bruce Allen <CodeAmend@Gmail.Com>
2016-01-12 13:51:21 -0500

    writing out menu logic

--- menu.py
+++ menu.py
@@ -1,10 +1,25 @@
+from database import Database
+from models.blog import Blog
+
 
 class Menu(object):
 
     def __init__(self):
-        # ask for author name
-        # if author has account run
-        pass
+        self.user = input("Enter your author name: ")
+        if self._user_has_account():
+            print("Welcome back {}".format(self.user))
+        else:
+            self._create_user_account()
+
+    def _user_has_account(self):
+        return Database.find_one('blogs', {"author":self.user}) is not None
+
+    def _create_user_account(self):
+        title = input("Enter title: ")
+        description = input("Enter description: ")
+        blog = Blog(self.user, title, description)
+        blog.save_to_mongo()
+
 
     def run_menu(self):
         # User read or write a blog?
