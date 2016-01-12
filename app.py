13d6c66d58fc0c5f3ee620d4ee41373b702b5f8b
CodeAmend <CodeAmend@Gmail.Com>
2016-01-11 19:44:14 -0500

    basic files

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-import pymongo
+from database import Database
+from models.blog import Blog
 
-uri = "mongodb://127.0.0.1:27017"
-client = pymongo.MongoClient(uri)
-database = client['fullstack']
-collection = database['students']
+__author__ = 'jslvtr'
 
-studentList = [stud['name'] for stud in collection.find({})]
+Database.initialize()
 
-print(studentList)
+blog = Blog(author="Bruce Bros",
+            title="Blog of choice",
+            description="Just one great article after another")
+
+blog.new_post()
+
+blog.save_to_mongo()
+
+blog_data = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
+
--- models/Post.py
+++
@@ -1,5 +0,0 @@
-class Post:
-
-    def __init__(self):
-        self.title = "This is my title"
-        
