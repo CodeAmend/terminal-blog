f3be35b843672070b35f8d4e6c567fed900c8e66
Michael Bruce Allen <CodeAmend@Gmail.Com>
2016-01-12 23:17:32 -0500

    basic logic for menu, ready to start testing

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 from database import Database
+from menu import Menu
 from models.blog import Blog
 
 __author__ = 'jslvtr'
 
 Database.initialize()
 
-blog = Blog(author="Bruce Bros",
-            title="Blog of choice",
-            description="Just one great article after another")
-
-
-#new_post() asks us for information for new post. Creates post object and saves it with blog_id
-blog.new_post()
-#saves blog information to mongo
-blog.save_to_mongo()
-
-
-#blog_data = Blog.from_mongo(blog.id)
-
-print(blog.get_posts())
+Menu()
 
